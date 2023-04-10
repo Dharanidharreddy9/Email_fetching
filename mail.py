d2b2745afc2abba8284208b7662686ba5ffd3722
Dharanidharreddy9 <73034711+Dharanidharreddy9@users.noreply.github.com>
2023-04-10 08:37:39 +0530

    first commit

--- mail.py
+++ mail.py
@@ -1,119 +1,118 @@
 #-------------   OUTLOOK IMPLEMENTATION ---------------#
 
 
-import imaplib
-import email
-from bs4 import BeautifulSoup
-import email
-from email.header import decode_header
-import webbrowser
-import os
+# import imaplib
+# import email
+# from bs4 import BeautifulSoup
+# import email
+# from email.header import decode_header
+# import webbrowser
+# import os
 
-# account credentials
-username = "mrigendrasing@outlook.com"
-password = "Mrigendra@12"
+# # account credentials
+# username = "mrigendrasing@outlook.com"
+# password = "Mrigendra@12"
 
-imap_server = "outlook.office365.com"
+# imap_server = "outlook.office365.com"
 
-def clean(text):
-    # clean text for creating a folder
-    return "".join(c if c.isalnum() else "_" for c in text)
+# def clean(text):
+#     # clean text for creating a folder
+#     return "".join(c if c.isalnum() else "_" for c in text)
 
-# create an IMAP4 class with SSL 
-imap = imaplib.IMAP4_SSL(imap_server)
-# authenticate
-imap.login(username, password)
-status, messages = imap.select("INBOX")
-# number of top emails to fetch
-# status, messages = imap.list("INBOX")
-N = 1
-# total number of emails
-messages = int(messages[0])
-print(messages)
+# # create an IMAP4 class with SSL 
+# imap = imaplib.IMAP4_SSL(imap_server)
+# # authenticate
+# imap.login(username, password)
+# status, messages = imap.select("INBOX")
+# # number of top emails to fetch
+# # status, messages = imap.list("INBOX")
+# N = 1
+# # total number of emails
+# messages = int(messages[0])
+# print(messages)
 
 
-for i in range(messages, messages-N, -1):
-    # fetch the email message by ID
-    res, msg = imap.fetch(str(i), "(RFC822)")
-    for response in msg:
-        if isinstance(response, tuple):
-            # parse a bytes email into a message object
-            msg = email.message_from_bytes(response[1])
-            # decode the email subject
-            subject, encoding = decode_header(msg["Subject"])[0]
-            if isinstance(subject, bytes):
-                # if it's a bytes, decode to str
-                subject = subject.decode(encoding)
-            # decode email sender
-            From, encoding = decode_header(msg.get("From"))[0]
-            if isinstance(From, bytes):
-                From = From.decode(encoding)
-                print(From, "Sent:")
-            print("Subject:", subject)
-            print("From:", From)
-            # if the email message is multipart
-            if msg.is_multipart():
-                print("inside", "is_multipart")
-                # iterate over email parts
-                for part in msg.walk():
-                    # extract content type of email
-                    content_type = part.get_content_type()
-                    # print(content_type, "content_type")
-                    content_disposition = str(part.get("Content-Disposition"))
-                    # print(content_disposition, "content_disposition")
-                    try:
-                        # get the email body
-                        body = part.get_payload(decode=True).decode()
-                        soup = BeautifulSoup(body, 'html.parser')
+# for i in range(messages, messages-N, -1):
+#     # fetch the email message by ID
+#     res, msg = imap.fetch(str(i), "(RFC822)")
+#     for response in msg:
+#         if isinstance(response, tuple):
+#             # parse a bytes email into a message object
+#             msg = email.message_from_bytes(response[1])
+#             # decode the email subject
+#             subject, encoding = decode_header(msg["Subject"])[0]
+#             if isinstance(subject, bytes):
+#                 # if it's a bytes, decode to str
+#                 subject = subject.decode(encoding)
+#             # decode email sender
+#             From, encoding = decode_header(msg.get("From"))[0]
+#             if isinstance(From, bytes):
+#                 From = From.decode(encoding)
+#                 print(From, "Sent:")
+#             print("Subject:", subject)
+#             print("From:", From)
+#             # if the email message is multipart
+#             if msg.is_multipart():
+#                 print("inside", "is_multipart")
+#                 # iterate over email parts
+#                 for part in msg.walk():
+#                     # extract content type of email
+#                     content_type = part.get_content_type()
+#                     # print(content_type, "content_type")
+#                     content_disposition = str(part.get("Content-Disposition"))
+#                     # print(content_disposition, "content_disposition")
+#                     try:
+#                         # get the email body
+#                         body = part.get_payload(decode=True).decode()
+#                         soup = BeautifulSoup(body, 'html.parser')
 
-                        # Extract the "From" address from the email using the email module
-                        msg = email.message_from_string(soup.prettify())
-                        from_address = msg['Sent']
+#                         # Extract the "From" address from the email using the email module
+#                         msg = email.message_from_string(soup.prettify())
+#                         from_address = msg['Sent']
 
-                        # Print the "From" address
-                        print(from_address, "Sent")
-                        # print(body, "body")
-                    except Exception:
-                        pass
-                    if content_type == "text/plain" and "attachment" not in content_disposition:
-                        # print text/plain emails and skip attachments
-                        print(body)
-                    elif "attachment" in content_disposition:
-                        # download attachment
-                        filename = part.get_filename()
-                        if filename:
-                            folder_name = clean(subject)
-                            if not os.path.isdir(folder_name):
-                                # make a folder for this email (named after the subject)
-                                os.mkdir(folder_name)
-                            filepath = os.path.join(folder_name, filename)
-                            # download attachment and save it
-                            open(filepath, "wb").write(part.get_payload(decode=True))
-            else:
-                # extract content type of email
-                content_type = msg.get_content_type()
-                # get the email body
-                body = msg.get_payload(decode=True).decode()
-                if content_type == "text/plain":
-                    # print only text email parts
-                    print(body)
-            if content_type == "text/html":
-                # if it's HTML, create a new HTML file and open it in browser
-                folder_name = clean(subject)
-                if not os.path.isdir(folder_name):
-                    # make a folder for this email (named after the subject)
-                    os.mkdir(folder_name)
-                filename = "index.html"
-                filepath = os.path.join(folder_name, filename)
-                # write the file
-                open(filepath, "w").write(body)
-                # open in the default browser
-                # webbrowser.open(filepath)
-            # print("="*100)
-# close the connection and logout
-imap.close()
-imap.logout()
-
+#                         # Print the "From" address
+#                         print(from_address, "Sent")
+#                         # print(body, "body")
+#                     except Exception:
+#                         pass
+#                     if content_type == "text/plain" and "attachment" not in content_disposition:
+#                         # print text/plain emails and skip attachments
+#                         print(body)
+#                     elif "attachment" in content_disposition:
+#                         # download attachment
+#                         filename = part.get_filename()
+#                         if filename:
+#                             folder_name = clean(subject)
+#                             if not os.path.isdir(folder_name):
+#                                 # make a folder for this email (named after the subject)
+#                                 os.mkdir(folder_name)
+#                             filepath = os.path.join(folder_name, filename)
+#                             # download attachment and save it
+#                             open(filepath, "wb").write(part.get_payload(decode=True))
+#             else:
+#                 # extract content type of email
+#                 content_type = msg.get_content_type()
+#                 # get the email body
+#                 body = msg.get_payload(decode=True).decode()
+#                 if content_type == "text/plain":
+#                     # print only text email parts
+#                     print(body)
+#             if content_type == "text/html":
+#                 # if it's HTML, create a new HTML file and open it in browser
+#                 folder_name = clean(subject)
+#                 if not os.path.isdir(folder_name):
+#                     # make a folder for this email (named after the subject)
+#                     os.mkdir(folder_name)
+#                 filename = "index.html"
+#                 filepath = os.path.join(folder_name, filename)
+#                 # write the file
+#                 open(filepath, "w").write(body)
+#                 # open in the default browser
+#                 # webbrowser.open(filepath)
+#             # print("="*100)
+# # close the connection and logout
+# imap.close()
+# imap.logout()
 
 
 
@@ -265,34 +264,34 @@ def read_message(service, message):
             if name.lower() == "to":
                 # we print the To address
                 print("To:", value)
-            # if name.lower() == "subject":
+            if name.lower() == "subject":
                 # make our boolean True, the email has "subject"
-                # has_subject = True
+                has_subject = True
                 # make a directory with the name of the subject
                 # folder_name = clean(value)
                 # we will also handle emails with the same subject name
-                # folder_counter = 0
-                # while os.path.isdir(folder_name):
-                #     folder_counter += 1
-                #     # we have the same folder name, add a number next to it
-                #     if folder_name[-1].isdigit() and folder_name[-2] == "_":
-                #         folder_name = f"{folder_name[:-2]}_{folder_counter}"
-                #     elif folder_name[-2:].isdigit() and folder_name[-3] == "_":
-                #         folder_name = f"{folder_name[:-3]}_{folder_counter}"
-                #     else:
-                #         folder_name = f"{folder_name}_{folder_counter}"
-                # os.mkdir(folder_name)
-                # print("Subject:", value)
-            # if name.lower() == "date":
-            #     # we print the date when the message was sent
-            #     print("Date:", value)
-    # if not has_subject:
-    #     # if the email does not have a subject, then make a folder with "email" name
-    #     # since folders are created based on subjects
-    #     if not os.path.isdir(folder_name):
-    #         os.mkdir(folder_name)
+                folder_counter = 0
+                while os.path.isdir(folder_name):
+                    folder_counter += 1
+                    # we have the same folder name, add a number next to it
+                    if folder_name[-1].isdigit() and folder_name[-2] == "_":
+                        folder_name = f"{folder_name[:-2]}_{folder_counter}"
+                    elif folder_name[-2:].isdigit() and folder_name[-3] == "_":
+                        folder_name = f"{folder_name[:-3]}_{folder_counter}"
+                    else:
+                        folder_name = f"{folder_name}_{folder_counter}"
+                os.mkdir(folder_name)
+                print("Subject:", value)
+            if name.lower() == "date":
+                # we print the date when the message was sent
+                print("Date:", value)
+    if not has_subject:
+        # if the email does not have a subject, then make a folder with "email" name
+        # since folders are created based on subjects
+        if not os.path.isdir(folder_name):
+            os.mkdir(folder_name)
     # parse_parts(service, parts, folder_name, message)
-    # print("="*50)
+    print("="*50)
 
 
 
